fc03bef049a56a34548094dc9d984b4b31aaedb1
RoUchiha <swaggsikh@gmail.com>
2022-03-23 01:18:57 -0500

    added methods to WorkoutsAdapter.

--- frontend/src/adapters/workoutsAdapter.js
+++ frontend/src/adapters/workoutsAdapter.js
@@ -4,5 +4,35 @@ class WorkoutsAdapter {
         this.baseUrl = "http://localhost:3000/workouts"
     }
 
+    getWorkouts() {
+        return fetch(this.baseUrl).then(res => res.json())
+    }
 
+    createWorkout(name, date) {
+        const workoutData ={
+            name: name,
+            date: date
+        }
+
+        return fetch(this.baseUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(workoutData)
+        }).then(res => res.json())
+    }
+
+
+    deleteWorkout(workoutId) {
+        const workoutDelete = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        return fetch(`${this.baseUrl}/${workoutId}`, workoutDelete).then(res => res.json())
+    }
+
+    
 }
